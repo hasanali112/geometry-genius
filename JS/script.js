4cbff5313868a469dfc847ca5c0c63321b942586
hasanali112 <mdhasan.ru.ro@gmail.com>
2023-02-18 23:23:46 +0600

    pantagon and eventlisten add

--- JS/script.js
+++ JS/script.js
@@ -69,3 +69,47 @@ document.getElementById("rectanglur-calculate-btn").addEventListener("click", fu
     showCalculation.appendChild(tr);
 
 })
+
+document.getElementById("parallelogram-calculate-btn").addEventListener("click", function(){
+
+    const  areaCalculation= getAreaValues("parallelogram-base-value", "parallelogram-height-value")
+    //  show the value in area calculation
+    const showCalculation = document.getElementById('area-calculation');
+    const tr = document.createElement('tr');
+    tr.innerHTML = `
+    <td>3.</td>
+    <td>Parallelogram</td>
+    <td>${areaCalculation} Cm<sup>2</sup></td>
+    `;
+    showCalculation.appendChild(tr);
+
+})
+
+document.getElementById("rhombus-calculate-btn").addEventListener("click", function(){
+
+    const  areaCalculation= getArea("rhombus-base-value", "rhombus-height-value")
+    //  show the value in area calculation
+    const showCalculation = document.getElementById('area-calculation');
+    const tr = document.createElement('tr');
+    tr.innerHTML = `
+    <td>4.</td>
+    <td>Rhombus</td>
+    <td>${areaCalculation} Cm<sup>2</sup></td>
+    `;
+    showCalculation.appendChild(tr);
+})
+
+
+document.getElementById("pentagon-calculate-btn").addEventListener("click", function(){
+
+    const  areaCalculation= getArea("pentagon-base-value", "pentagon-height-value")
+    //  show the value in area calculation
+    const showCalculation = document.getElementById('area-calculation');
+    const tr = document.createElement('tr');
+    tr.innerHTML = `
+    <td>5.</td>
+    <td>Pentagon</td>
+    <td>${areaCalculation} Cm<sup>2</sup></td>
+    `;
+    showCalculation.appendChild(tr);
+})
